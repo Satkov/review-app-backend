44a344697cab0ddb8407fce9d19aefbbdc3bd3f6
Georgy Satkov <74203877+Satkov@users.noreply.github.com>
2022-01-16 15:38:56 +0300

    add redoc

--- reccy/reccy/settings.py
+++ reccy/reccy/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'users',
     'rest_framework',
     'django_filters'
